aa4940392a71bd00fd4777cfa8c21170dfbe1ffb
0x00a <paolo@async.ly>
2017-08-08 13:59:02 +0200

    frist

---
+++ README.md
@@ -0,0 +1,26 @@
+```
+__      __________/\________________
+  |    /         / /
+  |   /  __     / /   /
+  |  /  /\ \   | / ——/—— /__   __
+  | /  /  \/   /    /   /  /  /  \   /
+  |/   \__/\__/\___/\__/   \_/__/ \_/
+```
+
+[![Development sponsored by voltra.co](https://img.shields.io/badge/Development%20sponsored%20by-Voltra.co-yellow.svg)](https://voltra.co/)
+
+# SYNOPSIS
+Scroll window (or element) to Y, optinally animate it.
+
+```js
+const scrollToY = require('scrolltoy')
+
+const position = n
+const time = 200
+
+scrolltoy(window, position, time)
+```
+
+```js
+npm install voltraco/scrolltoy
+```
---
+++ index.js
@@ -0,0 +1,50 @@
+var requestFrame = (function () {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function requestAnimationFallback (callback) {
+      window.setTimeout(callback, 1000 / 60)
+    }
+})()
+
+function ease (pos) {
+  return ((pos /= 0.5) < 1)
+    ? (0.5 * Math.pow(pos, 5))
+    : (0.5 * (Math.pow((pos - 2), 5) + 2))
+}
+
+module.exports = function scrollToY (el, Y, speed) {
+  var isWindow = !!el.alert
+  var scrollY = isWindow ? el.scrollY : el.scrollTop
+  var pos = Math.abs(scrollY - Y)
+  var time = Math.max(0.1, Math.min(pos / speed, 0.8))
+
+  let currentTime = 0
+
+  function setY () {
+    currentTime += 1 / 60
+
+    var p = currentTime / time
+    var t = ease(p)
+
+    if (p < 1) {
+      var y = scrollY + ((Y - scrollY) * t)
+      requestFrame(setY)
+
+      if (isWindow) {
+        el.scrollTo(0, y)
+      } else {
+        el.scrollTop = y
+      }
+
+      return
+    }
+
+    if (isWindow) {
+      el.scrollTo(0, Y)
+    } else {
+      el.scrollTop = Y
+    }
+  }
+  setY()
+}
